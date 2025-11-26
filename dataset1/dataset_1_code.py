2b3d858fb377e4fbeabab02a31793417246cff1a
hikarisuwan <hikari.suwa24@imperial.ac.uk>
2025-11-26 22:14:04 +0000

    Fix comment casing for dataset 1 pipeline

--- dataset1/dataset_1_code.py
+++ dataset1/dataset_1_code.py
@@ -12,7 +12,7 @@ from ml_classes import run_full_analysis
 def main() -> None:
     csv_path = os.path.join(current_dir, 'dataset_1.csv')
     
-    # Run pipeline for Dataset 1
+    # Run pipeline for dataset 1
     run_full_analysis(
         dataset_path=csv_path,
         output_dir_name='outputs_ds1',
