1293d2b1a45baee8420ea335897dce885d439e0a
hikarisuwan <hikari.suwa24@imperial.ac.uk>
2025-11-27 21:42:01 +0000

    Clarify comment on adding parent directory to sys.path
    Updated comment to clarify the addition of the parent directory to sys.path.

--- dataset1/dataset_1_code.py
+++ dataset1/dataset_1_code.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-# Add parent directory to path to import ml_classes
+# we added the parent directory to sys.path to import 'ml_classes'
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 if parent_dir not in sys.path:
